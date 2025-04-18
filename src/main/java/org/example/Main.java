931613539215dd62233784ca7f3422d581fd9613
Jonathan Velasquez <johedvelca@gmail.com>
2025-04-18 00:41:47 -0400

    simple server built, running and serving on 127.0.0.1:8080

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,24 @@
 package org.example;
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.SimpleFileServer;
+
+import java.net.InetSocketAddress;
+import java.nio.file.Path;
 
 public class Main {
     public static void main(String[] args) {
+        try {
+            Path path = Path.of("C:\\Users\\johed\\Desktop\\Java\\functional-server-library\\build");
+            path.toFile().mkdirs();
 
-        System.out.println("Hello, World!");
-        System.out.println("Test 1");
+            InetSocketAddress address = new InetSocketAddress(8080);
+            HttpServer server = SimpleFileServer.createFileServer(address, path, SimpleFileServer.OutputLevel.VERBOSE);
+            System.out.println("Server started on port 8080, serving files from: " + path.toAbsolutePath());
+            server.start();
+        } catch (Exception e) {
+            System.err.println("Error starting server: " + e.getMessage());
+            e.printStackTrace();
+            System.exit(1);
+        }
     }
 }
